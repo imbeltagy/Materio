58b308e78bec7d5a13dcc99f119e9c2d1b92dacb
Mohammed-Beltagy <justbeltagy@gmail.com>
2023-11-12 20:26:46 +0200

    Fixed useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Container, CssBaseline, Stack, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo, useState } from "react";
 import "./assets/fonts.css";
-import Sidebar from "./components/sidebar/Index";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/sidebar/Index";
 import Header from "./components/header/Index";
-import { useState } from "react";
 
 const Layout = () => {
   return (
@@ -21,54 +21,55 @@ function App() {
   const [mode, setMode] = useState("dark");
 
   const theme = useMemo(
-    createTheme({
-      typography: {
-        fontFamily: "Inter, sans-serif",
-      },
-      palette: {
-        primary: {
-          light: "rgb(198, 167, 254)",
-          main: "rgb(145, 85, 253)",
+    () =>
+      createTheme({
+        typography: {
+          fontFamily: "Inter, sans-serif",
         },
-        secondary: {
-          main: "rgb(138, 141, 147)",
-        },
-        success: {
-          main: "rgb(86, 202, 0)",
-        },
-        error: {
-          main: "rgb(255, 76, 81)",
-        },
-        warning: {
-          main: "rgb(255, 180, 0)",
-        },
-        info: {
-          main: "rgb(22, 177, 255)",
-        },
+        palette: {
+          primary: {
+            light: "rgb(198, 167, 254)",
+            main: "rgb(145, 85, 253)",
+          },
+          secondary: {
+            main: "rgb(138, 141, 147)",
+          },
+          success: {
+            main: "rgb(86, 202, 0)",
+          },
+          error: {
+            main: "rgb(255, 76, 81)",
+          },
+          warning: {
+            main: "rgb(255, 180, 0)",
+          },
+          info: {
+            main: "rgb(22, 177, 255)",
+          },
 
-        ...(mode === "light"
-          ? {
-              background: {
-                paper: "#fff",
-                default: "#f4f5fa",
-              },
-              text: {
-                primary: "rgba(58, 53, 65, 0.87)",
-                secondary: "rgba(58, 53, 65, 0.68)",
-              },
-            }
-          : {
-              background: {
-                paper: "rgb(49, 45, 75)",
-                default: "rgb(40, 36, 61)",
-              },
-              text: {
-                primary: "rgba(231, 227, 252, 0.87)",
-                secondary: "rgba(231, 227, 252, 0.68)",
-              },
-            }),
-      },
-    }),
+          ...(mode === "light"
+            ? {
+                background: {
+                  paper: "#fff",
+                  default: "#f4f5fa",
+                },
+                text: {
+                  primary: "rgba(58, 53, 65, 0.87)",
+                  secondary: "rgba(58, 53, 65, 0.68)",
+                },
+              }
+            : {
+                background: {
+                  paper: "rgb(49, 45, 75)",
+                  default: "rgb(40, 36, 61)",
+                },
+                text: {
+                  primary: "rgba(231, 227, 252, 0.87)",
+                  secondary: "rgba(231, 227, 252, 0.68)",
+                },
+              }),
+        },
+      }),
     [mode]
   );
 
