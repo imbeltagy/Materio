3a803c750c245d363f41d801ea6d0ffd17ca7bcc
imbeltagy <justbeltagy@gmail.com>
2023-12-26 12:00:11 +0200

    style: moved some rules from dashboard page to global

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Box, Container, CssBaseline, Grid, Stack, ThemeProvider, createTheme } 
 import { Suspense, lazy, useMemo, useState } from "react";
 import "./assets/fonts.css";
 import "./scrollbar.css";
+import "./global.css";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/sidebar/Index";
 import Header from "./components/header/Index";
@@ -26,72 +27,71 @@ function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const [mode, setMode] = useState("light");
-
-  const theme = useMemo(
-    () =>
-      createTheme({
-        typography: {
-          fontFamily: "Inter, sans-serif",
+  const theme = useMemo(() => {
+    const modifiedTheme = createTheme({
+      typography: {
+        fontFamily: "Inter, sans-serif",
+      },
+      palette: {
+        mode,
+        primary: {
+          light: "rgb(198, 167, 254)",
+          main: "rgb(145, 85, 253)",
         },
-        palette: {
-          mode,
-          primary: {
-            light: "rgb(198, 167, 254)",
-            main: "rgb(145, 85, 253)",
-          },
-          secondary: {
-            light: "rgba(58, 53, 65, 0.1)",
-            main: "rgb(138, 141, 147)",
-          },
-          success: {
-            main: "rgb(86, 202, 0)",
-          },
-          error: {
-            main: "rgb(255, 76, 81)",
-          },
-          warning: {
-            main: "rgb(255, 180, 0)",
-          },
-          info: {
-            main: "rgb(22, 177, 255)",
-          },
-
-          ...(mode === "light"
-            ? {
-                background: {
-                  paper: "#fff",
-                  default: "#f4f5fa",
-                },
-                text: {
-                  primary: "rgba(58, 53, 65, 0.87)",
-                  secondary: "rgba(58, 53, 65, 0.68)",
-                  disabled: "#dddee3",
-                  light: "rgba(58, 53, 65, 0.38)",
-                },
-                divider: "rgba(58, 53, 65, 0.1)",
-              }
-            : {
-                background: {
-                  paper: "rgb(49, 45, 75)",
-                  default: "rgb(40, 36, 61)",
-                },
-                text: {
-                  primary: "rgba(231, 227, 252, 0.87)",
-                  secondary: "rgba(231, 227, 252, 0.68)",
-                  disabled: "#444059",
-                  light: "#605c75",
-                },
-                action: {
-                  active: "#fff",
-                  hover: "rgba(231, 227, 252, 0.04)",
-                },
-                divider: "#605c7560",
-              }),
+        secondary: {
+          light: "rgba(58, 53, 65, 0.1)",
+          main: "rgb(138, 141, 147)",
         },
-        spacing: (i) => `${i * 0.5}rem`,
-      }),
-    [mode]
-  );
+        success: {
+          main: "rgb(86, 202, 0)",
+        },
+        error: {
+          main: "rgb(255, 76, 81)",
+        },
+        warning: {
+          main: "rgb(255, 180, 0)",
+        },
+        info: {
+          main: "rgb(22, 177, 255)",
+        },
+
+        ...(mode === "light"
+          ? {
+              background: {
+                paper: "#fff",
+                default: "#f4f5fa",
+              },
+              text: {
+                primary: "rgba(58, 53, 65, 0.87)",
+                secondary: "rgba(58, 53, 65, 0.68)",
+                disabled: "#dddee3",
+                light: "rgba(58, 53, 65, 0.38)",
+              },
+              divider: "rgba(58, 53, 65, 0.1)",
+            }
+          : {
+              background: {
+                paper: "rgb(49, 45, 75)",
+                default: "rgb(40, 36, 61)",
+              },
+              text: {
+                primary: "rgba(231, 227, 252, 0.87)",
+                secondary: "rgba(231, 227, 252, 0.68)",
+                disabled: "#444059",
+                light: "#605c75",
+              },
+              action: {
+                active: "#fff",
+                hover: "rgba(231, 227, 252, 0.04)",
+              },
+              divider: "#605c7560",
+            }),
+      },
+      spacing: (i) => `${i * 0.5}rem`,
+    });
+    modifiedTheme.shadows[1] = "0px 2px 10px 0px rgba(58, 53, 65, 0.1)";
+    return modifiedTheme;
+  }, [mode]);
 
   const routes = {
     withSidebar: [
