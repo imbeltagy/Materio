98cdd389208e7f990a40d7b756f6407fe0757ace
imbeltagy <justbeltagy@gmail.com>
2023-12-26 12:49:45 +0200

    style: added tooltip to submit button on accountSettings page

--- src/pages/accountSettings/components/FormActions.jsx
+++ src/pages/accountSettings/components/FormActions.jsx
@@ -1,11 +1,13 @@
-import { Button, Stack } from "@mui/material";
+import { Button, Stack, Tooltip } from "@mui/material";
 
 const FormActions = () => {
   return (
     <Stack direction="row" gap={2}>
-      <Button variant="contained" type="submit">
-        save changes
-      </Button>
+      <Tooltip title="Console.log form data" arrow>
+        <Button variant="contained" type="submit">
+          save changes
+        </Button>
+      </Tooltip>
       <Button variant="outlined" color="secondary" type="reset">
         reset
       </Button>
